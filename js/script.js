47db6d1756c7cc8c51b51589f95c0ac16b618eae
Javier <javipons@mail.com>
2020-03-23 18:12:04 -0300

    carousel responsive

--- js/script.js
+++ js/script.js
@@ -6,4 +6,37 @@ jQuery(document).ready(function($){
         //$('nav').slideToggle();
         //$('nav').animate({width:'toggle'},350);
     });
+
+    // Todas las funciones se ejecutan al cambiar el tamaño de la pantalla
+    $(window).resize(function() {
+        if ($(window).width() >= 576) {
+            // INICIO FUNCION CAMBIAR IMAGENES BANNER EN MOBIL CON RESPECTO A LAPTOP
+            // Borramos todos los slides del container
+            $(".carousel-container-js").empty();
+            for(var i = 1; i <= 3; i++){
+                // si es la primera foto , en div class agregar active
+                if(i == 1) {
+                    $(".carousel-container-js" ).append( `<div class="carousel-item active"><img src="img/banner${i}.jpg" class="d-block w-100 img-carousel" alt="imagen banner ${i}"></div>` );
+                } else {
+                    $(".carousel-container-js" ).append( `<div class="carousel-item "><img src="img/banner${i}.jpg" class="d-block w-100 img-carousel" alt="imagen banner ${i}"></div>` );
+                }
+            }
+        } else {
+            $(".carousel-container-js").empty();
+            for(var i = 1; i <= 3; i++){
+                // si es la primera foto , en div class agregar active
+                if(i == 1) {
+                    $(".carousel-container-js" ).append( `<div class="carousel-item active"><img src="img/banner-mobile${i}.jpg" class="d-block w-100 img-carousel" alt="imagen banner ${i}"></div>` );
+                } else {
+                    $(".carousel-container-js" ).append( `<div class="carousel-item "><img src="img/banner-mobile${i}.jpg" class="d-block w-100 img-carousel" alt="imagen banner ${i}"></div>` );
+                }
+            }
+            
+        }
+        // Cuando hacemos resize que tambien cambie el espacio del top del sitio
+        var height1 = $('.main-header').height();
+        var height2 = $('.top-header').height();
+        $('body').css('margin-top',(height1 + height2) + 'px');
+
+    }).resize();
 });
